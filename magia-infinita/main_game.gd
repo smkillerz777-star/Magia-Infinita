4068deba5d74e7af7750003ddfd9cb4286bd1ec0
smkillerz777-star <s.mkillerz777@gmail.com>
2026-06-20 23:36:37 +0530

    add function resample()
    this function will take a line as input and convert it into the specified number of vertices this is the first step for symbol recognization

--- magia-infinita/main_game.gd
+++ magia-infinita/main_game.gd
@@ -10,6 +10,7 @@ func _process(_delta):
 			$lines.get_child(i).queue_free()
 		elif(is_complete()):
 			$lines.get_child(i).default_color = Color(0,0,0,1)
+			#resample($lines.get_child(i))
 			activate($lines.get_child(i))
 			i+=1
 		else:
@@ -25,10 +26,43 @@ func _on_inventory_pressed() -> void:
 	add_child(load("res://inventory.tscn").instantiate())
 
 func activate(line: Line2D):
-	$lines.remove_child(line)
 	get_child(0).visible = false
 	get_child(1).visible = false
 	get_child(2).visible = false
 	get_child(3).visible = true
 	get_child(3).get_child(2).visible = true
-	get_child(3).get_child(2).get_child(0).add_child(line)
+	var new = resample(line,63)
+	get_child(3).get_child(2).get_child(0).add_child(new)
+	for point in new.points:
+		print(point)
+
+#takes a line as an input and make the total vectice count to number
+func resample(line: Line2D,number):
+	var new_line : Line2D = Line2D.new()
+	var temp : PackedVector2Array = PackedVector2Array()
+	var path_length = 0.0
+	var j = 1
+	for point in line.points:
+		if(j==line.points.size()):
+			path_length += point.distance_to(line.points[0])
+			break
+		path_length += point.distance_to(line.points[j])
+		j+=1
+	temp.resize(number)
+	var dis = path_length/number
+	var d = 0.0
+	j = 1
+	var k=1
+	temp[0] = line.points[0]
+	for point in line.points:
+		if(j==line.points.size()):
+			break
+		d += point.distance_to(line.points[j])
+		if(d>dis):
+			temp[k] = point
+			k+=1
+			d=d-dis
+		j+=1
+	temp.append(line.points[0])
+	new_line.points = temp
+	return new_line
