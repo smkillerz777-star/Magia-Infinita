a0807d9d350a9e2b98e17dc11a25d49078476358
smkillerz777-star <s.mkillerz777@gmail.com>
2026-06-20 16:33:37 +0530

    added functionality to buttons in information,add a container lines
    now all lines drawn will be child of lines container, pressing cancel in information will remove the information panel

--- magia-infinita/information.gd
+++ magia-infinita/information.gd
@@ -1,1 +1,9 @@
 extends Control
+
+
+
+func _on_use_pressed() -> void:
+	print("use")
+
+func _on_cancel_pressed() -> void:
+	queue_free()
--- magia-infinita/main_game.gd
+++ magia-infinita/main_game.gd
@@ -1,18 +1,18 @@
-extends Node2D
-var i = 1
+extends Control
+var i = 0
 func _process(deltall):
 	if(Input.is_action_just_pressed("draw")):
-		add_child(Line2D.new())
+		$lines.add_child(Line2D.new())
 	elif(Input.is_action_pressed("draw")):
-		get_child(i).add_point((get_global_mouse_position()))
+		$lines.get_child(i).add_point((get_global_mouse_position()))
 	elif(Input.is_action_just_released("draw")):
 		if(is_complete()):
-			get_child(i).queue_free()
+			$lines.get_child(i).queue_free()
 			i-=1
 		i+=1
 	else:
-		print(get_children().size())
+		pass
 func is_complete():
-	if(get_child(i).points[0].distance_to(get_child(i).points[get_child(i).points.size()-1])<5):
+	if($lines.get_child(i).points[0].distance_to($lines.get_child(i).points[$lines.get_child(i).points.size()-1])<5):
 			print("complete by itself")
 			return true
