465c595d262754910751570dcd1b541e3e941257
smkillerz777-star <s.mkillerz777@gmail.com>
2026-06-20 17:01:33 +0530

    added 3d main_game and activate()
    now when the spell is complete the game will go to the 3d mode to show the animation of the spell

--- magia-infinita/main_game.gd
+++ magia-infinita/main_game.gd
@@ -6,10 +6,16 @@ func _process(deltall):
 	elif(Input.is_action_pressed("draw")):
 		$lines.get_child(i).add_point((get_global_mouse_position()))
 	elif(Input.is_action_just_released("draw")):
-		if(is_complete()):
+		print($lines.get_child(i).points.size())
+		if($lines.get_child(i).points.size()<30):
+			print("deleted")
+			$lines.get_child(i).queue_free()
+		elif(is_complete()):
 			$lines.get_child(i).default_color = Color(0,0,0,1)
-			i-=1
-		i+=1
+			activate($lines.get_child(i))
+			i+=1
+		else:
+			i+=1
 	else:
 		pass
 func is_complete():
@@ -20,3 +26,6 @@ func is_complete():
 
 func _on_inventory_pressed() -> void:
 	add_child(load("res://inventory.tscn").instantiate())
+
+func activate(_line):
+	get_tree().change_scene_to_file("res://3d_main_game.tscn")
