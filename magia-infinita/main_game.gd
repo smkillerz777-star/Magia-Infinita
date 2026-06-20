437f5637c7528cbfcd2fb9b26c7ede6719a2cdb7
smkillerz777-star <s.mkillerz777@gmail.com>
2026-06-20 14:58:57 +0530

    added the wakatime and drawing function
    now the player can draw in the screen

--- magia-infinita/main_game.gd
+++ magia-infinita/main_game.gd
@@ -1,1 +1,11 @@
 extends Node2D
+var i = 0
+func _process(deltall):
+	if(Input.is_action_just_pressed("draw")):
+		add_child(Line2D.new())
+	elif(Input.is_action_pressed("draw")):
+		get_child(i).add_point((get_global_mouse_position()))
+	elif(Input.is_action_just_released("draw")):
+		i+=1
+	else:
+		pass
