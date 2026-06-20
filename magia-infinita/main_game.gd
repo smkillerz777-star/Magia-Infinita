ed1416c9d4719c7f1c9f17a437449198354e62e8
smkillerz777-star <s.mkillerz777@gmail.com>
2026-06-20 21:40:25 +0530

    added subviewport to 3d,logic to activate()
    now when the circle is activated the line that is completed will become child of sub viewport and that sub viewport will become texture of box mesh

--- magia-infinita/main_game.gd
+++ magia-infinita/main_game.gd
@@ -1,14 +1,12 @@
 extends Control
 var i = 0
-func _process(deltall):
+func _process(_delta):
 	if(Input.is_action_just_pressed("draw")):
 		$lines.add_child(Line2D.new())
 	elif(Input.is_action_pressed("draw")):
 		$lines.get_child(i).add_point((get_global_mouse_position()))
 	elif(Input.is_action_just_released("draw")):
-		print($lines.get_child(i).points.size())
 		if($lines.get_child(i).points.size()<30):
-			print("deleted")
 			$lines.get_child(i).queue_free()
 		elif(is_complete()):
 			$lines.get_child(i).default_color = Color(0,0,0,1)
@@ -20,12 +18,17 @@ func _process(deltall):
 		pass
 func is_complete():
 	if($lines.get_child(i).points[0].distance_to($lines.get_child(i).points[$lines.get_child(i).points.size()-1])<5):
-			print("complete by itself")
 			return true
 
 
 func _on_inventory_pressed() -> void:
 	add_child(load("res://inventory.tscn").instantiate())
 
-func activate(_line):
-	get_tree().change_scene_to_file("res://3d_main_game.tscn")
+func activate(line: Line2D):
+	$lines.remove_child(line)
+	get_child(0).visible = false
+	get_child(1).visible = false
+	get_child(2).visible = false
+	get_child(3).visible = true
+	get_child(3).get_child(2).visible = true
+	get_child(3).get_child(2).get_child(0).add_child(line)
