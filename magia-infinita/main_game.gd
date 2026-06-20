904f350c8b0d18f399fa3429a49e55ef6d2fe00a
smkillerz777-star <s.mkillerz777@gmail.com>
2026-06-20 15:57:53 +0530

    added inventory,global,is_complete()
    base the of the inventory is added where the player can store various items they need for drawing magic circle. Now if a circle is complete it gets deleted automatically later in the development, instead of delete the circle will activate . added a global script that will keep things are required across scene

--- magia-infinita/main_game.gd
+++ magia-infinita/main_game.gd
@@ -1,11 +1,18 @@
 extends Node2D
-var i = 0
+var i = 1
 func _process(deltall):
 	if(Input.is_action_just_pressed("draw")):
 		add_child(Line2D.new())
 	elif(Input.is_action_pressed("draw")):
 		get_child(i).add_point((get_global_mouse_position()))
 	elif(Input.is_action_just_released("draw")):
+		if(is_complete()):
+			get_child(i).queue_free()
+			i-=1
 		i+=1
 	else:
 		pass
+func is_complete():
+	if(get_child(i).points[0].distance_to(get_child(i).points[get_child(i).points.size()-1])<5):
+			print("complete by itself")
+			return true
