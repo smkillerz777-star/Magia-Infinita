ef66b94f0e10a83b578be3fb18f15a013f21476d
smkillerz777-star <s.mkillerz777@gmail.com>
2026-06-20 16:47:33 +0530

    updated inventory,main_game
    inventory now has a cancel button, its size is now smaller instaed of full rect and magic line will become black instead of disappearing when complete

--- magia-infinita/inventory.gd
+++ magia-infinita/inventory.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_node("/root/main_game").add_child(load("res://information.tscn").instantiate())
+
+
+func _on_cancel_pressed() -> void:
+	queue_free()
--- magia-infinita/main_game.gd
+++ magia-infinita/main_game.gd
@@ -7,7 +7,7 @@ func _process(deltall):
 		$lines.get_child(i).add_point((get_global_mouse_position()))
 	elif(Input.is_action_just_released("draw")):
 		if(is_complete()):
-			$lines.get_child(i).queue_free()
+			$lines.get_child(i).default_color = Color(0,0,0,1)
 			i-=1
 		i+=1
 	else:
@@ -16,3 +16,7 @@ func is_complete():
 	if($lines.get_child(i).points[0].distance_to($lines.get_child(i).points[$lines.get_child(i).points.size()-1])<5):
 			print("complete by itself")
 			return true
+
+
+func _on_inventory_pressed() -> void:
+	add_child(load("res://inventory.tscn").instantiate())
