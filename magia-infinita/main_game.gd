7881d135b21658ab43d3aeec786b1676a5cf0a5c
smkillerz777-star <s.mkillerz777@gmail.com>
2026-06-22 19:08:56 +0530

    added real time line, circle detection
    added line straihg that draws line, is straight() a function that checks line

--- magia-infinita/main_game.gd
+++ magia-infinita/main_game.gd
@@ -1,24 +1,37 @@
 extends Control
 var i = 0
 var completed = 0
+var first = true
+var category = null
 func _process(_delta):
 	if(Input.is_action_just_pressed("draw")):
+		category = null
 		$lines.add_child(Line2D.new())
 	elif(Input.is_action_pressed("draw")):
 		$lines.get_child(i).add_point((get_global_mouse_position()))
+		if($lines.get_child(i).points.size()>40):
+			if(category==null):
+				category = get_category($lines.get_child(i))
+			else:
+				if(get_category($lines.get_child(i)) != category):
+					$lines.get_child(i).default_color = Color(0,0,0,1)
+					category = null
+					$lines.add_child(Line2D.new())
+					i+=1
+
 	elif(Input.is_action_just_released("draw")):
 		if($lines.get_child(i).points.size()<30):
 			$lines.get_child(i).queue_free()
 		elif(is_complete()):
 			var line = $lines.get_child(i)
 			line.default_color = Color(0,0,0,1)
-			print(is_circle(line))
 			#$lines.remove_child(line)
 			#$completed.add_child(line)
 			#add_child( normalization(resample(line),300))
 			i+=1
 		else:
 			i+=1
+	print(category)
 	if($completed.get_child_count()>(completed+1)):
 		if($completed.get_child_count()>=2):
 			completed+=1
@@ -98,20 +111,21 @@ func line_size(line):
 		min_x = min(min_x,point.x)
 		min_y = min(min_y,point.y)
 	return max(max_x-min_x,max_y-min_y)
+
 #this function compares the two lines that are input and written they being same chances
 func compare(line1,line2,does_size_matter=false,does_orientation_matter=false):
 	var min_dis = 0.2 * 32
 	var new_line1 = resample(line1)
 	var new_line2 = resample(line2)
+	if(not does_orientation_matter):
+		new_line1 = rotate_to(new_line1)
+		new_line2 = rotate_to(new_line2)
 	if(not does_size_matter):
 		new_line1 = normalization(new_line1)
 		new_line2 = normalization(new_line2)
 	else:
 		new_line1 = translate_to(new_line1)
 		new_line2 = translate_to(new_line2)
-	if(not does_orientation_matter):
-		new_line1 = rotate_to(new_line1)
-		new_line2 = rotate_to(new_line2)
 	var dis = 0.0
 	for j in new_line1.points.size():
 		dis += new_line1.points[j].distance_to(new_line2.points[j])
@@ -129,6 +143,7 @@ func glow(mesh,subviewport):
 		mat.emission_texture = viewport_texture
 		mat.emission = Color(0.0, 0.5, 1.0)
 		mat.emission_energy_multiplier = 4
+
 func mid_point(line):
 	var max_x = -INF
 	var max_y = -INF
@@ -140,8 +155,10 @@ func mid_point(line):
 		min_x = min(min_x,point.x)
 		min_y = min(min_y,point.y)
 	return Vector2((max_x+min_x)/2,(max_y+min_y)/2)
+
 func angle_bet(point1,point2):
 	return atan2(point1.y-point2.y,point1.x-point2.x)
+
 func rotate_to(line,deg=0):
 	var about = mid_point(line)
 	var temp = []
@@ -154,6 +171,7 @@ func rotate_to(line,deg=0):
 	var new_line = Line2D.new()
 	new_line.points = temp
 	return new_line
+
 func is_dot(line):
 	if(line_size(line)<15):
 		return true
@@ -187,6 +205,7 @@ func change_size(line,multiplier):
 	var new_line = Line2D.new()
 	new_line.points = temp
 	return new_line
+
 func translate_to(line):
 	var min_x = INF
 	var min_y = INF
@@ -203,3 +222,24 @@ func translate_to(line):
 	var new_line = Line2D.new()
 	new_line.points = temp
 	return new_line
+
+func line_straight(angle=0,length=50):
+	var temp = []
+	temp.resize(2)
+	temp[0] = Vector2(0,0)
+	temp[1] = Vector2(sin(deg_to_rad(angle))*length,cos(deg_to_rad(angle))*length)
+	var new_line = Line2D.new()
+	new_line.points = temp
+	return new_line
+
+func is_straight(line):
+	if(compare(line,line_straight())>0.85):
+		return true
+	return false
+
+func get_category(line):
+	if(is_straight(line)):
+		return "line"
+	elif(is_circle(line)):
+		return "circle"
+	return null
