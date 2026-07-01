743865fcd825d0fafa584d269d986c851cf7aba7
smkillerz777-star <s.mkillerz777@gmail.com>
2026-07-01 06:14:33 +0530

    fixed the logic for player movement
    added gravity, made floor static body, made player movement normalized in horizontal direction but in y direction it is independent

--- magia-infinita/scripts/player.gd
+++ magia-infinita/scripts/player.gd
@@ -1,19 +1,24 @@
 extends CharacterBody3D
 @export var speed = 5
+@export var gravity = 15
 func _process(delta: float) -> void:
-    if(Input.is_action_just_pressed("forward")):
-        velocity.z = -1
-    if(Input.is_action_just_pressed("backward")):
-        velocity.z = 1
-    if(Input.is_action_just_released("forward") or Input.is_action_just_released("backward")):
-        velocity.z = 0
-    if(Input.is_action_just_pressed("left")):
-        velocity.x = -1
-    if(Input.is_action_just_pressed("right")):
-        velocity.x = 1
-    if(Input.is_action_just_released("left") or Input.is_action_just_released("right")):
-        velocity.x = 0
-    if(Input.is_action_just_pressed("jump")):
-        velocity.y = 1
-    velocity = velocity.normalized()*speed
-    move_and_slide()
+	if(Input.is_action_just_pressed("forward")):
+		velocity.z = -1
+	if(Input.is_action_just_pressed("backward")):
+		velocity.z = 1
+	if(Input.is_action_just_released("forward") or Input.is_action_just_released("backward")):
+		velocity.z = 0
+	if(Input.is_action_just_pressed("left")):
+		velocity.x = -1
+	if(Input.is_action_just_pressed("right")):
+		velocity.x = 1
+	if(Input.is_action_just_released("left") or Input.is_action_just_released("right")):
+		velocity.x = 0
+	if(Input.is_action_just_pressed("jump")):
+		velocity.y = speed
+	if(position.y>0.0):
+		velocity.y = velocity.y - gravity * delta
+	var hori_vel = Vector2(velocity.x,velocity.z)
+	hori_vel = hori_vel.normalized()* speed
+	velocity = Vector3(hori_vel.x,velocity.y,hori_vel.y)
+	move_and_slide()
