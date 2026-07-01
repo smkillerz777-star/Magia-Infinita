c5d63860575ae1b21652633c34aa873f37d34dd3
smkillerz777-star <s.mkillerz777@gmail.com>
2026-07-01 22:58:26 +0530

    added drawing function in main_game_3d
    now as the player moves the lines get automatically drawn now the i will use the function main_game here

--- magia-infinita/scripts/3d_main_game.gd
+++ magia-infinita/scripts/3d_main_game.gd
@@ -2,14 +2,16 @@ extends Node3D
 var i = 1
 func _ready() -> void:
 	var mat = $floor/mesh.get_active_material(0) as StandardMaterial3D
+	$SubViewport.add_child(Line2D.new())
 	if(mat):
 		mat.albedo_texture = $SubViewport.get_texture()
 		pass
 func _process(delta: float) -> void:
 	#$SubViewport/ColorRect.color.r = (1+int($SubViewport/ColorRect.color.r))%255
 	#var mat = $floor/mesh.get_active_material(0) as StandardMaterial3D
-	$SubViewport.add_child(Line2D.new())
-	var line = $SubViewport.get_child(i) as Line2D
-	line.add_point(Vector2(720-i,720))
-	line.add_point(Vector2(719-i,720))
-	i+=1
+	if($player/view/ray.is_colliding()):
+		#$SubViewport.add_child(Line2D.new())
+		print(Vector2($player/view/ray.get_collision_point().x,$player/view/ray.get_collision_point().z))
+		var line = $SubViewport.get_child(i) as Line2D
+		line.add_point(Vector2($player/view/ray.get_collision_point().x,$player/view/ray.get_collision_point().z)*72)
+		#i+=1
